434357af9d0f31dd30ac990c824cb305e7038561
William Matton <williammatton@gmail.com>
2026-08-20 15:04:11 +0200

    Add basic menu interface and exit command handling.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Main {
-    static void main() {
-        IO.println("Hello World! This project uses signed commits.");
+    void main() {
+        IO.print("""
+                Elpriser – Analysverktyg
+                ========================
+                1. Välj elområde (SE1, SE2, SE3, SE4)
+                2. Min, Max och Medelpris
+                3. Sortera priser (lägst till högst)
+                4. Bästa laddningstid (4h sammanhängande)
+                e. Avsluta
+                """);
+        String command;
+        do {
+            command = IO.readln("Kommando: ");
+            switch (command) {
+                case "1":
+                    break;
+                default:
+                    break;
+            }
+        } while (!isExitCommand(command));
+    }
+
+    boolean isExitCommand(String command) {
+        return Objects.equals(command, "e") || Objects.equals(command, "E");
     }
 }
