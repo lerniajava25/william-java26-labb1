5a9876f68167dada0b859aa9a1e96c957dafd784
William Matton <williammatton@gmail.com>
2026-08-23 14:18:14 +0200

    Remove unused `printCommandMenu` method.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,18 +26,6 @@ public class Main {
         IO.println("Hello World!");
     }
 
-    void printCommandMenu() {
-        IO.print("""
-                Elpriser – Analysverktyg
-                ========================
-                1. Välj elområde (SE1, SE2, SE3, SE4)
-                2. Min, Max och Medelpris
-                3. Sortera priser (lägst till högst)
-                4. Bästa laddningstid (4h sammanhängande)
-                e. Avsluta
-                """);
-    }
-
     TimeSlotPrice[] choosePriceZone() {
         try {
             String priceZone;
